32c3b93fc73763ec68be557d090ae63e6b311433
warlock <lvelezsantos@gmail.com>
2013-09-20 09:11:41 -0500

    Modificado livesettings Modificado inventory form modificado blackbird manager para que no use media_url y use static_url agregado archivo setup

---
+++ setup.py
@@ -0,0 +1,33 @@
+# coding=utf-8
+
+import os
+from setuptools import setup
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-satchmo-grappelli',
+    version='0.1',
+    packages=['satchmo_grappelli'],
+    include_package_data=True,
+    license='MIT License',  # example license
+    description='Skin for satchmo',
+    long_description=README,
+    url='https://github.com/lvelezsantos/django-satchmo-grappelli',
+    author='Luis Velez',
+    author_email='lvelezsantos@gmail.com',
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        "Framework :: Django",
+
+    ],
+)
